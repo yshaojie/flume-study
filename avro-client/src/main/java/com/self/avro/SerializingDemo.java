cda070795d4acf29f6a3881e1e4e74dc2311ba3b
shaojieyue <xxdd@gmail.com>
2015-09-06 08:37:57 +0800

    commit

---
+++ avro-client/src/main/java/com/self/avro/SerializingDemo.java
@@ -0,0 +1,39 @@
+package com.self.avro;
+
+import org.apache.avro.file.DataFileWriter;
+import org.apache.avro.io.DatumWriter;
+import org.apache.avro.specific.SpecificDatumWriter;
+
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * Created by shaojieyue on 9/5/15.
+ */
+public class SerializingDemo {
+    public static void main(String[] args) throws IOException {
+        User user1 = new User();
+        user1.setName("Alyssa");
+        user1.setFavoriteNumber(256);
+        // Leave favorite color null
+
+        // Alternate constructor
+        User user2 = new User("Ben", 7, "red");
+
+        // Construct via builder
+        User user3 = User.newBuilder()
+                .setName("Charlie")
+                .setFavoriteColor("blue")
+                .setFavoriteNumber(null)
+                .build();
+
+        // Serialize user1, user2 and user3 to disk
+        DatumWriter<User> userDatumWriter = new SpecificDatumWriter<User>(User.class);
+        DataFileWriter<User> dataFileWriter = new DataFileWriter<User>(userDatumWriter);
+        dataFileWriter.create(user1.getSchema(), new File("users.avro"));
+        dataFileWriter.append(user1);
+        dataFileWriter.append(user2);
+        dataFileWriter.append(user3);
+        dataFileWriter.close();
+    }
+}
